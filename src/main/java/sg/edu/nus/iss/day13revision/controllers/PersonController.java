7c34bc07dbaabb635222d8313bfd7895da370c4a
Yimin Zheng <yiminzheng29@gmail.com>
2022-07-21 14:09:23 +0800

    added addPerson.html

--- src/main/java/sg/edu/nus/iss/day13revision/controllers/PersonController.java
+++ src/main/java/sg/edu/nus/iss/day13revision/controllers/PersonController.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import sg.edu.nus.iss.day13revision.models.Person;
+import sg.edu.nus.iss.day13revision.models.PersonForm;
 import sg.edu.nus.iss.day13revision.services.PersonService;
 
 @Controller
@@ -47,6 +49,34 @@ public class PersonController {
         model.addAttribute("persons", personList);
 
         return "personList";
+    }
+
+    @RequestMapping(value="/addPerson", method=RequestMethod.GET)
+    public String showAddPersonPage(Model model) {
+        
+        PersonForm pForm = new PersonForm();
+        model.addAttribute("personForm", pForm);
+
+
+        return "addPerson";
+    }
+
+    @RequestMapping(value="/addPerson", method=RequestMethod.POST, produces="application/json") // method cannot be the same as the above (get)
+    public String savePerson(Model model, 
+    @ModelAttribute("personForm") PersonForm personForm) { // as the items are passed as objects (in the addPerson.html), don't need to use multivaluemap here. Instead, objects are passed as ModelAttributes
+
+        String fName = personForm.getFirstName();
+        String lName = personForm.getLastName();
+        
+
+        if (fName!=null && fName.length()>0 && lName!=null && lName.length()>0) {
+            Person newPerson = new Person(fName, lName);
+            perSvc.addPerson(newPerson);
+
+            return "redirect:/personList"; // call get/personList, retrieve records and displays them in the html page
+        } 
+        model.addAttribute("errorMessage", errorMessage);
+        return "addPerson";
     }
 
 }
